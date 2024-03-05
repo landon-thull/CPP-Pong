e7e963fdf4744192ee8dd223319e69a6ac26df00
Landon Thull <landonthull.tx@gmail.com>
2024-03-05 12:19:11 -0800

    add ball and paddles

--- main.cpp
+++ main.cpp
@@ -3,6 +3,24 @@
 
 using namespace std;
 
+class Ball {
+public:
+float x, y;
+int speed_x, speed_y;
+int radius;
+
+void Draw() {
+    DrawCircle(x, y, radius, WHITE);
+}
+
+void Update() {
+    x += speed_x;
+    y += speed_y;
+}
+};
+
+Ball ball;
+
 int main () {
     cout << "Starting the game" << endl;
 
@@ -11,9 +29,25 @@ int main () {
     InitWindow(screen_width, screen_height, "My Pong Game!");
     SetTargetFPS(60);
 
+    ball.radius = 20;
+    ball.x = screen_width / 2;
+    ball.y = screen_height / 2;
+    ball.speed_x = 7;
+    ball.speed_y = 7;
+
     while (WindowShouldClose() == false) {
         BeginDrawing();
 
+        // updating
+        ball.Update();
+
+        // drawing
+        // center ball in screen
+        ClearBackground(BLACK);
+        DrawLine(screen_width / 2, 0, screen_width / 2, screen_height, WHITE);
+        ball.Draw();
+        DrawRectangle(10, screen_height / 2 - 60, 25, 120, BLUE);
+        DrawRectangle(screen_width - 35, screen_height / 2 - 60, 25, 120, RED);
         EndDrawing();
     }
 
