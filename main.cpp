b120dcc3244185fca98d5f71089ce9e136a1aea3
Landon Thull <landonthull.tx@gmail.com>
2024-03-05 12:23:31 -0800

    add edge collision detection

--- main.cpp
+++ main.cpp
@@ -16,6 +16,13 @@ void Draw() {
 void Update() {
     x += speed_x;
     y += speed_y;
+
+    if (y + radius >= GetScreenHeight() || y - radius <= 0) {
+        speed_y *= -1;
+    }
+    if (x + radius >= GetScreenWidth() || x - radius <= 0) {
+        speed_x *= -1;
+    }
 }
 };
 
