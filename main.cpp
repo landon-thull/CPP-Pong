8ef6f4f23334a7f9b0bbd44288205511c0ca1020
Landon Thull <landonthull.tx@gmail.com>
2024-03-05 12:07:51 -0800

    create window and game loop

--- main.cpp
+++ main.cpp
@@ -6,5 +6,17 @@ using namespace std;
 int main () {
     cout << "Starting the game" << endl;
 
+    const int screen_width = 1280;
+    const int screen_height = 800;
+    InitWindow(screen_width, screen_height, "My Pong Game!");
+    SetTargetFPS(60);
+
+    while (WindowShouldClose() == false) {
+        BeginDrawing();
+
+        EndDrawing();
+    }
+
+    CloseWindow();
     return 0;
 }
